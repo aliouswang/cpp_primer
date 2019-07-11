3ecd385addf2f03126d21cff2a1f6ed568cf9750
aliouswang <wsw622@126.com>
2019-07-11 12:30:08 +0800

    Initial Commit

---
+++ cpp_primer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp_primer
+//
+//  Created by aliouswang on 2019/7/11.
+//  Copyright © 2019 aliouswang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
